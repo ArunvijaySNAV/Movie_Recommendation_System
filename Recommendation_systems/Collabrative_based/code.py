2a7a2cc20bc293a7297d4de5eb4ac298600818dc
Arunvijay <arunvijaysnav@gmail.com>
2026-05-12 23:30:23 +0530

    Update code.py

--- Recommendation_systems/Collabrative_based/code.py
+++ Recommendation_systems/Collabrative_based/code.py
@@ -1,1 +1,28 @@
+# load libraries
 
+import pandas as pd
+
+ratings = pd.read_csv('ratings.csv')
+
+ratings = ratings[['userId', 'movieId', 'rating']]
+
+!pip install scikit-surprise
+
+trainset = dataset.build_full_trainset()
+
+# Model definition
+
+from surprise import SVD
+
+svd = SVD()
+
+svd.fit(trainset)
+svd.predict(1, 1061)
+
+actual_data = pd.read_csv('ratings.csv')
+
+from surprise import model_selection
+
+cross_validation = model_selection.cross_validate(svd, data=dataset, measures=['rmse'])
+
+cross_validation
